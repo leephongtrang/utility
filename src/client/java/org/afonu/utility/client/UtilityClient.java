87c0c2fe32083dce93c8e42fc32f7b48bfe0b579
Afonu <dmo11256@gmail.com>
2024-12-03 19:13:57 -0500

    project start-up

---
+++ src/client/java/org/afonu/utility/client/UtilityClient.java
@@ -0,0 +1,10 @@
+package org.afonu.utility.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UtilityClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/afonu/utility/Utility.java
@@ -0,0 +1,10 @@
+package org.afonu.utility;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Utility implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
