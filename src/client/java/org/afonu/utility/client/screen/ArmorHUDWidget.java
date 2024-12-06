b05667bd70f22d43449e79fca98c311aed95321c
dvp536 <missdegat@hotmail.com>
2024-12-05 21:58:46 -0500

    Add toggle to ArmorHUD

--- src/client/java/org/afonu/utility/client/screen/ArmorHUDWidget.java
+++ src/client/java/org/afonu/utility/client/screen/ArmorHUDWidget.java
@@ -7,6 +7,7 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.item.ItemStack;
+import org.afonu.utility.client.option.OptionInit;
 
 import java.awt.*;
 
@@ -15,22 +16,24 @@ import static org.afonu.utility.client.option.OptionInit.Option;
 public class ArmorHUDWidget implements HudRenderCallback {
     @Override
     public void onHudRender(DrawContext drawContext, RenderTickCounter renderTickCounter) {
-        MinecraftClient client = MinecraftClient.getInstance();
+        if (Option.toggleArmorHUD) {
+            MinecraftClient client = MinecraftClient.getInstance();
 
-        assert MinecraftClient.getInstance() != null;
-        ClientPlayerEntity player = MinecraftClient.getInstance().player;
+            assert MinecraftClient.getInstance() != null;
+            ClientPlayerEntity player = MinecraftClient.getInstance().player;
 
-        assert player != null;
-        Iterable<ItemStack> armor = player.getArmorItems();
-        TextRenderer renderer = client.textRenderer;
+            assert player != null;
+            Iterable<ItemStack> armor = player.getArmorItems();
+            TextRenderer renderer = client.textRenderer;
 
-        int relativePosition = 0;
+            int relativePosition = 0;
 
-        for (ItemStack itemStack : armor) {
-            if (itemStack.getMaxDamage() != 0) {
-                drawContext.drawItem(itemStack, Option.armorHUD_positionX, Option.armorHUD_positionY + relativePosition);
-                drawContext.drawText(renderer, String.valueOf(itemStack.getMaxDamage() - itemStack.getDamage()), Option.armorHUD_positionX + 20, Option.armorHUD_positionY + relativePosition + 4, itemStack.getItemBarColor(), true);
-                relativePosition -= 20;
+            for (ItemStack itemStack : armor) {
+                if (itemStack.getMaxDamage() != 0) {
+                    drawContext.drawItem(itemStack, Option.armorHUD_positionX, Option.armorHUD_positionY + relativePosition);
+                    drawContext.drawText(renderer, String.valueOf(itemStack.getMaxDamage() - itemStack.getDamage()), Option.armorHUD_positionX + 20, Option.armorHUD_positionY + relativePosition + 4, itemStack.getItemBarColor(), true);
+                    relativePosition -= 20;
+                }
             }
         }
     }
